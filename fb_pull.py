7e386cf5623b12d3a807340bd6e9257a3264e19f
Drew Abbott <abbotta4@gmail.com>
2021-03-28 01:46:05 -0400

    Complete v10 update
    Only 89 pictures pulled with first test. It looks like photos uploaded
    by other people for the most part are not able to be accessed.

--- fb_pull.py
+++ fb_pull.py
@@ -1,5 +1,5 @@
 import requests, json, piexif, os, logging
-from PIL import Image as image
+#from PIL import Image as image
 from configparser import ConfigParser
 from datetime import datetime
 
@@ -16,13 +16,15 @@ ACCESS_TOKEN = config['DEFAULT']['access_token']
 def get_request_uri(api_call):
     return BASE_URI + api_call + "&access_token=" + ACCESS_TOKEN
 
-def setup_dirs():
+def setup_dir():
     r = requests.get(get_request_uri("me?fields=name"))
     j = json.loads(r.text)
     fb_name = j['name']
+    dirname = "photos of " + fb_name
 
     try:
-        os.mkdir(fb_name)
+        os.mkdir(dirname)
+        os.chdir(dirname)
     except FileExistsError:
         pass
     except OSError:
@@ -34,38 +36,41 @@ def get_photos():
     r = requests.get(get_request_uri("me/photos?type=tagged&fields=images,backdated_time,created_time,from,name,name_tags,place")) #adding album eliminates photos?
     j = json.loads(r.text)
     photos = j['data']
-    #images = j['data']['images']
-    while 'foo' not in j:
-        #logging.info(photos.keys())
-        #logging.info(photos)
+    nphotos = 0
+    while True:
+        logging.info(photos)
         for photo in photos:
             photo_uri = photo['images'][0]['source'] # assumes first image in 'images' is the highest res
             time = photo['created_time'] if 'backdated_time' not in photo else photo['backdated_time']
             time = datetime.strptime(time, '%Y-%m-%dT%H:%M:%S%z').strftime('%Y%m%d%H%M%S')
             photographer = photo['from']['name']
-            description = photo['name']
+            description = False if 'name' not in photo else photo['name']
             #album = photo['album']
             #album_name = album['name']
             #album_ctime = datetime.strptime(album['created_time'], '%Y-%m-%dT%H:%M:%S%z').strftime('%Y%m%d%H%M%S')
             #album_dir = fb_name + '/' + album_ctime + '_' + album_name
             try:
-                os.mkdir(album_dir)
+                os.mkdir(photographer)
             except FileExistsError:
                 pass
             except OSError:
-                print ("Creation of the directory %s failed" % album_dir)
-                exit(1)
-            link = photo['link']
-            with open(album_dir + '/links.txt', 'a') as f:
-                f.write(link + '\n')
+                print ("Creation of the directory {} failed".format(album_dir))
+                return -1
+            with open(photographer + '/' + time, 'wb') as f:
+                r = requests.get(photo_uri)
+                f.write(r.content)
                 nphotos += 1
         try:
-            request_uri = photos['paging']['next']
+            request_uri = j['paging']['next']
         except KeyError:
-            break
+            #break
+            return nphotos
         r = requests.get(request_uri)
-        logging.info(r)
+        logging.info("getting 'next': " + str(r))
         j = json.loads(r.text)
-        photos = j
+        photos = j['data']
 
-print('dumped %d links' % links)
+if __name__ == "__main__":
+#    if not setup_dir():
+        #get_photos()
+        print("dumped {} photos".format(get_photos()))
